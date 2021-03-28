df349c97e0691be071a653bd8440fa23c751b826
Niko Vertovec <nikolaus.vertovec@eng.ox.ac.uk>
2021-03-28 10:29:06 -0700

    added multiobjective optimization part

--- GoKart_example.m
+++ GoKart_example.m
@@ -2,9 +2,10 @@
 addpath(genpath('../Add-Ons/level-set-methods-toolbox/Kernel/'));
 
 %% Should we compute the trajectory?
-compTraj = true;
-dims = [1, 2, 3, 4];
-projDim = [1 1 0 0];
+compTraj = false;
+stopInit = false;
+dims     = [1, 2, 3, 4];
+projDim  = [1, 1, 0, 0];
 
 %% Grid
 grid_min = [0.0; -3.5; 0.1; -0.1]; % Lower corner of computation domain
@@ -23,7 +24,7 @@ data0 = shapeSphere(g, [0; 0; 1; 1], R);
 
 %% time vector
 t0 = 0;
-tMax = 4;
+tMax = 3;
 dt = 0.01;
 tau = t0:dt:tMax;
 
@@ -51,7 +52,9 @@ schemeData.accuracy = 'veryHigh'; %set accuracy
 schemeData.uMode = uMode;
 schemeData.dIn = [0; 0; 0];
 
-HJIextraArgs.stopInit = x_0(dims);
+if stopInit
+    HJIextraArgs.stopInit = x_0(dims);
+end
 mask = shapeRectangleByCorners(g, min_bnd(dims), max_bnd(dims));
 HJIextraArgs.obstacleFunction = -mask;
 
@@ -70,8 +73,18 @@ HJIextraArgs.visualize.plotData.projpt = ones(g.dim-2,1); %project at ...
 %[data, tau, extraOuts] = ...
 % HJIPDE_solve(data0, tau, schemeData, minWith, extraArgs)
 [data, tau2, ~] = ...
-  HJIPDE_solve(data0, tau, schemeData, 'none', HJIextraArgs);
+  HJIPDE_solve(data0, tau, schemeData, 'zero', HJIextraArgs);
 
+%% Compute Pareto Front
+lb = max(grid_min(3:4),min_bnd(3:4));
+ub = min(grid_max(3:4),max_bnd(3:4));
+options = optimoptions(@gamultiobj,'PlotFcn',@gaplotpareto);
+if length(dims) == 4
+    save('nonlcon_variables.mat', '-v7.3', 'g', 'data', 'x_0')
+    [x,fval,exitflag,output,population,scores] = gamultiobj(@J,2,[],[],[],[],lb,ub,@nonlcon,options);
+else
+    [x,fval,exitflag,output,population,scores] = gamultiobj(@J,2,[],[],[],[],lb,ub,@nonlcon_split,options);
+end 
 %% Compute optimal trajectory from some initial state
 if compTraj
   
@@ -116,8 +129,6 @@ if compTraj
     %flip data time points so we start from the beginning of time
     dataTraj = flip(data,g.dim+1);
     
-    % [traj, traj_tau] = ...
-    % computeOptTraj(g, data, tau, dynSys, extraArgs)
     [traj, traj_tau] = ...
       computeOptTraj(g, dataTraj, tau2, dCar, TrajextraArgs);
     
@@ -136,4 +147,36 @@ if compTraj
   else
     error(['Initial state is not in the BRS/BRT! It have a value of ' num2str(value,2)])
   end
+end
+
+%% Additional functions
+
+function y = J(x)
+    y(1) = x(1);
+    y(2) = x(2);
+end
+
+function [c, ceq] = nonlcon(y)
+    % use save('nonlcon_variables.mat', '-v7.3', 'g', 'data', 'x_0')
+    persistent g data x_0
+    if isempty(x_0)
+        load('nonlcon_variables.mat');
+    end
+    % Compute nonlinear inequalities at y.
+    c = [eval_u(g, data(:,:,:,:,end), [x_0(1:2); y(1); y(2)], 'linear')];
+    % Compute nonlinear equalities at y.
+    ceq = [];
+end
+
+function [c, ceq] = nonlcon_split(y)
+    % use save('nonlcon_split_variables.mat', '-v7.3', 'g_3', 'g_4', 'data_3', 'data_4', 'x_0')
+    persistent g_3 g_4 data_3 data_4 x_0
+    if isempty(x_0)
+        load('nonlcon_split_variables.mat');
+    end
+    % Compute nonlinear inequalities at y.
+    c = [eval_u(g_3, data_3(:,:,:,end), [x_0(1:2); y(1)], 'linear'); ...
+            eval_u(g_4, data_4(:,:,:,end), [x_0(1:2); y(2)], 'linear')];
+    % Compute nonlinear equalities at y.
+    ceq = [];
 end
